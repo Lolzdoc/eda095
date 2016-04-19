a5aac009476722abe4a4d7b31ae35abbd0dd31cc
Per-Victor Persson <dracyr@gmail.com>
2016-04-19 14:22:33 +0200

    Renamed ChatSession -> Server

--- Lab_3/src/Server/Final/Main.java
+++ Lab_3/src/Server/Final/Main.java
@@ -3,19 +3,17 @@ package Server.Final;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String args[]) {
-        ArrayList<Socket> sockets = new ArrayList<>();
-        ChatSession session = new ChatSession();
+        Server server = new Server();
 
         try {
             ServerSocket serverSocket = new ServerSocket(30000);
             while (true) {
                 Socket socket = serverSocket.accept();
-                session.addParticipant(socket);
+                server.addParticipant(socket);
                 System.out.println("[INFO] Client connected!");
             }
         } catch (IOException e) {
